d88af25e1f36749825a2c944f24f5f0968a56162
Sunny Bhadani <sunnybhadani7@gmail.com>
2020-05-19 08:17:34 +0700

    Add files via upload
    covid tracker additional files

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Container from 'components/Container';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <p>
+          <a href="https://www.linkedin.com/in/sunny-bhadani-40b1a8112/">Made by @sunnybhadani</a>
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
